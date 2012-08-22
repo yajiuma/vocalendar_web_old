c1c08f4b14d8113a44c958612360d0a43019ccf4
yajiuma <masaru.nakabayashi@gmail.com>
2012-08-22 20:55:04 +0900

    フッタ部分を共通モジュールにまとめた

--- about/index.php
+++ about/index.php
@@ -139,43 +139,12 @@
 		</footer>
 		
 	</div><!-- EOD:#vocalendarCNT //-->
-		
-		<!-- ナビゲーション //-->
-		<aside id="VCLnavi">
-			<div id="VCLnaviCNT">
-				<div id="VCLnaviContainer">
-					<aside id="VCLnaviLogo"></aside>
-					<section id="VCLstatus">
-						<!-- Twitterボタン //-->
-						<aside id="VCLtweetBtn">
-							<a href="https://twitter.com/share" class="twitter-share-button" data-text="ボカロ関連イベントの共有カレンダー【 VOCALENDAR | ボカレンダー 】" data-hashtags="vocalendar" data-url="http://vocalendar.jp/" data-related="vocalendar">Tweet</a>
-							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-						</aside>
-						<aside id="VCLlikeBtn">
-							<div class="fb-like" data-href="http://vocalendar.jp/" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>
-							<!--<a href="https://twitter.com/VOCALENDAR" class="follow-button" data-show-count="false" data-lang="ja" target="_blank"><span class="bold">@VOCALENDAR</span>をフォロー</a>//-->
-						</aside>
-						<aside id="VCLglpusBtn">
-							<g:plusone size="medium" href="http://vocalendar.jp/"></g:plusone>
-						</aside>
-					</section><!-- EOD:#VCLstatus //-->
-					
-					<!-- 更新状況 //-->
-					<aside id="VCLcounter">
-						有志編集メンバー：<strong>35人</strong> / 登録イベント数：<strong><span id="item-num"></span>件</strong>
-					</aside>
-					<div id="actionBTN"><a id="actionBTNon"></a></div>
-				</div>
-				<section id="bannerCNT" class="clearfix">
-					<a href="http://bit.ly/vcl90005" target="_blank"><img src="/images/banners/vocalendar-banner-gumitan2012.jpg" alt="" style="margin-right: 10px;" /></a>
-					<img src="/images/banners/vocalendar-banner-regular-s.gif" alt="" style="box-shadow:none;" />
-				</section>
-			</div>
-		</aside>
-		
-		
-	<!-- VOCALENDAR JS読み込み //-->
-	<script src="../js/vocalendar.js"></script>
+
+<!-- フッター読み込み //-->
+<?php include "../modules/footer.tpl"; ?>
+
+<!-- VOCALENDAR JS読み込み //-->
+<script src="../js/vocalendar.js"></script>
 	
 </body>
 </html>
