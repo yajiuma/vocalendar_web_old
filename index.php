6e7a8d442ee77656261c5d15cdd05f45c7b875bf
yajiuma <masaru.nakabayashi@gmail.com>
2014-01-03 16:11:30 +0900

    α版コミット

--- index.php
+++ index.php
@@ -1,37 +1,59 @@
-<?php
-$smart_ua_list = array(
-	'iPhone',    // Apple iPhone
-	'iPod',    // Apple iPod touch
-	'Android',    // Android
-	'dream',    // Pre 1.5 Android
-	'CUPCAKE',    // 1.5+ Android
-	'blackberry',    // blackberry
-	'webOS',    // Palm Pre Experimental
-	'incognito',    // Other iPhone browser
-	'webmate',    // Other iPhone browser
-	'Kindle'
-);
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Bootstrap 101 Template</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/vocalendar.css" rel="stylesheet" media="screen">
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body id="vocalendar">
 
-$smart_mode = false;
+<div class="container-fluid">
 
-if ( empty($_GET['if'])) {
-	foreach ($smart_ua_list as $ua) {
-		if (stripos($_SERVER['HTTP_USER_AGENT'], $ua) === false) continue;
-		$smart_mode = true;
-		break;
-	}
-} elseif ( $_GET['if'] == "pc" ) {
-	$smart_mode = false;
-} elseif ( $_GET['if'] == "sp" ) {
-	$smart_mode = true;
-} else {
-	header("Status: 404 Not Found", true, 404);
-	echo "<h1>404 Not Found</h1>";
-	exit;
-}
+  <div class="navbar navbar-fixed-top navbar-inverse">
+    <div class="navbar-inner">
+    <div class="container-fluid">
+    <div class="row-fluid">
+    <div class="span12">
+        <a href="#" class="brand"><img src="images/vocalendar-small.gif" alt="VOCALENDAR"></a>
+        <ul class="nav">
+          <li><a href="/about/">About</a>
+          <li><a href="/gallery/">Gallery</a>
+          <li><a href="/blog/">Blog</a>
+          <li class="dropdown">
+            <a href="" class="dropdown-toggle" data-toggle="dropdown">
+              Music<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/miraikouro/">未来光路〜Miku ver.〜</a>
+            </ul>
+        </ul>
+        <form class="navbar-search pull-right">
+          <input type="text" class="search-query" placeholder="イベントを検索">
+        </form>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
 
-if (!$smart_mode) {
-	require_once ("index.tpl");
-} else {
-	require_once ("smart.tpl");
-}
+  <div class="row-fluid" id="maincontent">
+    <div class="span9">
+      <iframe id="vocalendar-gc" src="/gcalendar-wrapper-tsubaki.php?src=0mprpb041vjq02lk80vtu6ajgo%40group.calendar.google.com&amp;color=%230F4B38&amp;src=5fsoru1dfaga56mcleu5mp76kk%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Asia%2FTokyo&amp;showTitle=0&amp;showCalendars=1&amp;showTz=0&amp;showPrint=0&amp;height=650&amp;wkst=2&amp;bgcolor=%23FFFFFF" style=" border-width:0 " width="100%" height="650" frameborder="0" scrolling="no"></iframe>
+    </div>
+  </div>
+
+  <div class="row-fluid">
+      <div class="span12">
+        <section id="credits">
+        当サイト『VOCALENDAR』に掲載されている情報は個人利用の範囲でご自由にお使いいただいて結構ですが、商用利用はご遠慮ください。 / イラストはイラストレーターさんの大切な作品です。無断使用・転載などはくれぐれもおやめください。</span> / 当サイトで使用しているイラストはピアプロ・キャラクター・ライセンスに基いて<!--各社のキャラクターライセンスに基いて//-->クリプトン・フューチャー・メディア株式会社のキャラクター『初音ミク』を描いたものです。 / VOCALOIDならびにボーカロイドはヤマハ株式会社の登録商標です。 / 『MEIKO』『KAITO』『初音ミク』『鏡音リン』『鏡音レン』『巡音ルカ』はクリプトン・フューチャー・メディア株式会社の著作物です。 / 『歌愛ユキ』『氷山キヨテル』『SF-A2 開発コード miki』『猫村いろは』『結月ゆかり』は、株式会社AHSの登録商標です。 / 『Megpoid』『がくっぽいど』『Lily』『CUL』は株式会社インターネットの登録商標です。 / その他の製品・商品名は各社の商標または登録商標です。 / &copy; 2012 VOCALENDAR. Design by curioustander. Official illustration by ELrowa.
+       </section>
+      </div>
+  </div>
+
+</div>
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
